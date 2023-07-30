9295961b9693a90190e72123f46f61ad179ae7f5
bramhankmishra <bramhankmishra12b@gmail.com>
2023-07-30 19:45:33 +0530

    Date : 30/07/23

---
+++ output_in_java.java
@@ -0,0 +1,10 @@
+class output_in_java{
+    public static void main (String args[])
+    {
+        System.out.println("Hello, world");
+        System.out.println("Hello, world");
+        System.out.println("Hello, world");
+        System.out.println("Hello, world");
+        System.out.println("Hello, world");
+    }
+}
---
+++ printing_pattern.java
@@ -0,0 +1,11 @@
+// Print a star pattern
+public class printing_pattern {
+    public static void main(String args[] )
+    {
+        System.out.println("*****");
+        System.out.println("****");
+        System.out.println("***");
+        System.out.println("**");
+        System.out.println("*");
+    }
+}
