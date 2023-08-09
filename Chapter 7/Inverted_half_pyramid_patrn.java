7d8af83591cbf8350a9c4a212a7c74a445449942
Bramhank Mishra <112366701+bramhankmishra@users.noreply.github.com>
2023-08-09 09:52:33 +0530

    Add files via upload

---
+++ Chapter 7/Inverted_half_pyramid_patrn.java
@@ -0,0 +1,26 @@
+// Inverted and half pyramid pattern
+
+import java.util.*;
+
+public class Inverted_half_pyramid_patrn {
+    public static void pattern(int length){
+        for(int i = 1; i<=length; i++){
+            for(int j =1; j<=length; j++){
+                if(j>=length+1-i){
+                    System.out.print("*");
+                }
+                else{
+                    System.out.print(" ");
+                }
+            }
+            System.out.println("");
+        }
+    }
+
+    public static void main(String args[]) {
+        Scanner in = new Scanner(System.in);
+        System.out.print("Enter the legth of the rectangle you want to print = ");
+        int length = in.nextInt();
+        pattern(length);
+    }
+}
